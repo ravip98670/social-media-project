dd6586b3f993de3f841426f7b9f2f0712427a76a
ravip98670 <ravispandey2002@gmail.com>
2024-05-28 15:10:14 +0530

    Created_Header_Section_Using_Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import './App.css'
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
 function App() {
   return (
     <>
-     <div>Placeholder</div>
+      <Header> </Header>
     </>
   )
 }
